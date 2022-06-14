f2fd34c8bd3e7f61c173ab81a3f2f94ff2169d95
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-06-14 10:31:06 +0600

    feat[auth]: implement login system

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -9,9 +9,9 @@ const bcrypt = require('bcryptjs');
  */
 const register = async (req, res, next) => {
     // destructing required information for registration 
-    const {name, email , password } = req.body;
+    const { name, email, password } = req.body;
     // basic level validation
-    if (!name || !email|| !password) {
+    if (!name || !email || !password) {
         res.status(400).json({
             message: "Invalid Data"
         })
@@ -19,29 +19,70 @@ const register = async (req, res, next) => {
     }
     try {
         // check for user with same email address
-    let user = await User.findOne({email});
-    if (user) {
-        res.status(400).json({
-            message: "User already exist."
+        let user = await User.findOne({ email });
+        if (user) {
+            res.status(400).json({
+                message: "User already exist."
+            })
+            return;
+        }
+        // creating new user
+        user = new User({ name, email, password })
+        // hashing user password 
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+        user.password = hash;
+        await user.save();
+        delete user._doc.password;
+        delete user._doc._id;
+        delete user._doc.__v;
+        return res.status(201).json({
+            message: "user created successfully.",
+            user
         })
-        return;
-    }
-    // creating new user
-    user = new User({name,email,password})
-    // hashing user password 
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    user.password = hash;
-    await user.save();
-    return res.status(201).json({
-        message: "user created successfully.",
-        user
-    })
     } catch (error) {
         next(error)
     }
-} 
-
+}
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ * @param {express.NextFunction} next
+ */
+const login = async (req, res, next) => {
+    // destructing required information for registration 
+    const { email, password } = req.body;
+    // basic level validation
+    if (!email || !password) {
+        res.status(400).json({
+            message: "Invalid Data"
+        })
+        return;
+    }
+    try {
+        // check if there any user with that email
+        const user =await User.findOne({email});
+        if(!user) {
+            res.status(400).json({
+                message: "Invalid Credential"
+            })
+            return;
+        }
+        // match password with hashed form
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            res.status(400).json({
+                message: "Invalid Credential"
+            })
+            return;
+        }
+        // Todo: generate token
+        res.status(200).json({message: `You are logged in as ${user.name}`});
+    } catch (error) {
+        next(error)
+    }
+}
 module.exports = {
-    register
+    register, login
 }
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const {register} = require("../controller/auth.controller")
+const {register, login} = require("../controller/auth.controller")
 router.post("/register",register)
+router.post("/login",login)
 
 
 module.exports = router;
