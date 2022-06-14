e93c7bce35f3a1644879abe98ac0fdc22a616c40
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-06-14 14:00:40 +0600

    refactor: clean empty directories

--- server/app/middleware.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-
-const middleware = [morgan("dev"), cors(), express.json()];
-
-module.exports = middleware;
--- server/app/routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get("/health", (_req, res) => {
-  res.status(200).json({ message: "success" });
-});
-
-module.exports = router;
