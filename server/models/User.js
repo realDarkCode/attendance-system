c8fcdbd873e38452ac605c6746328fe315dbd3b1
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-06-14 15:01:14 +0600

    feat[USER-MODEL]: add schema level validation

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,40 @@
 const { model, Schema } = require("mongoose");
 
 const userSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
-  roles: [String],
-  accountStatus: String,
+  name: {
+    type: String, 
+    required: true, 
+    minlength: 3,
+    maxlength: 20, 
+  },
+  email: {
+    type: String, 
+    required: true, 
+    validate: {
+      validator: function(v) {
+        const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return emailRegex.test(v)
+      },
+      message:  props => `Invalid email: ${props.value}`
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, "Password is too short"],
+    maxlength: [30, "Password is too long"],
+  },
+  roles: {
+    type: [String],
+    required: true, 
+    default: ["STUDENT"],
+  },
+  accountStatus: {
+    type: String,
+    required: true,
+    enum: ["PENDING", "ACTIVE", "REJECTED"],
+    default: "PENDING"
+  },
 });
 const User = model("User", userSchema);
 module.exports = User;
