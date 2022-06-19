e0867aa1397e271733965795d743e20a34e3c091
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-06-19 17:09:07 +0600

    style: removed extra space

--- server/app/routes.js
+++ server/app/routes.js
@@ -3,6 +3,6 @@ const authRoute = require("../routes/auth.route");
 router.get("/health", (_req, res) => {
   res.status(200).json({ message: "success" });
 });
-router.use("/auth",authRoute);
+router.use("/auth", authRoute);
 
 module.exports = router;
